0448f840451a32f828c1610fde0c095e007f1b0b
dragonexpert <arceus@teamdimensional.net>
2022-09-08 15:49:09 -0500

    Added Update, Insert, and Delete Capabilities

--- QueryBuilder.java
+++ QueryBuilder.java
@@ -8,23 +8,28 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
+// Alter column syntax : Alter table tableName Alter column columnName TYPE type
 public class QueryBuilder
 {
-    Connection connection;
-    private String url;
-    boolean whereClauseSet;
-    boolean orderClauseSet;
-    String sql;
-    String columns;
-    String fromTable;
-    String whereClause;
-    String orderClause;
-    String limitClause;
-    ArrayList<Object> whereColumnValues;
-    ArrayList<String> columnType;
-    String className;
+    private final Connection connection;
+    private boolean whereClauseSet;
+    private boolean orderClauseSet;
+    private String sql;
+    private String columns;
+    private String fromTable;
+    private String whereClause;
+    private String orderClause;
+    private String limitClause;
+    private ArrayList<Object> whereColumnValues;
+    private ArrayList<String> columnType;
+    private String className;
+    private HashMap<String, Object> updateFields;
+    private String queryType;
+    private ResultSetMetaData resultSetMetaData;
+
 
     /**
      * Generic constructor.
@@ -43,6 +48,8 @@ public class QueryBuilder
         this.columnType = new ArrayList<>();
         this.className = className;
         this.connection = connection;
+        this.updateFields = new HashMap<>();
+        this.queryType = "select";
     }
 
     public boolean isWhereClauseSet()
@@ -130,6 +137,36 @@ public class QueryBuilder
         this.columnType = columnType;
     }
 
+    public HashMap<String, Object> getUpdateFields()
+    {
+        return updateFields;
+    }
+
+    public void setUpdateFields(HashMap<String, Object> updateFields)
+    {
+        this.updateFields = updateFields;
+    }
+
+    public String getQueryType()
+    {
+        return queryType;
+    }
+
+    public void setQueryType(String queryType)
+    {
+        this.queryType = queryType;
+    }
+
+    public ResultSetMetaData getResultSetMetaData()
+    {
+        return resultSetMetaData;
+    }
+
+    public void setResultSetMetaData(ResultSetMetaData resultSetMetaData)
+    {
+        this.resultSetMetaData = resultSetMetaData;
+    }
+
     /**
      * This allows you get columns separated by a comma.
      * @param columns CSV list of columns. * to get all columns.
@@ -138,6 +175,7 @@ public class QueryBuilder
     public QueryBuilder getColumns(String columns)
     {
         this.columns = columns;
+        setQueryType("select");
         return this;
     }
 
@@ -154,6 +192,7 @@ public class QueryBuilder
             this.columns += comma + c;
             comma = ", ";
         }
+        setQueryType("select");
         return this;
     }
 
@@ -164,7 +203,57 @@ public class QueryBuilder
     public QueryBuilder getColumns()
     {
         this.columns = "*";
+        setQueryType("select");
         return this;
+    }
+
+    // Generates the SQL for select statements.
+    private void selectQuery()
+    {
+        sql = "SELECT " + this.columns + " FROM " + this.fromTable + this.whereClause + this.orderClause + this.limitClause;
+        setSql(sql);
+    }
+
+    // Generates the SQL for update queries.
+    private void updateQuery()
+    {
+        String sql = "UPDATE " + getFromTable() + " SET ";
+        String comma = "";
+        for(String key : updateFields.keySet())
+        {
+            sql += comma + key + " = ?";
+            comma = ", ";
+        }
+        sql += getWhereClause();
+        setSql(sql);
+    }
+
+    // Generates the SQL for insert queries
+    private void insertQuery()
+    {
+        String sql = "INSERT INTO " + getFromTable() + "( ";
+        String comma = "";
+        for(String key : updateFields.keySet())
+        {
+            sql += comma + key;
+            comma = ", ";
+        }
+        comma = "";
+        sql += ") VALUES (";
+        for(int x = 0; x < updateFields.size(); x++)
+        {
+            sql += comma + "?";
+            comma = ", ";
+        }
+        sql += ")";
+        setSql(sql);
+    }
+
+    // Generate the SQL for delete queries
+    private void deleteQuery()
+    {
+        String sql = "DELETE FROM " + getFromTable() + getWhereClause();
+        setSql(sql);
     }
 
     /**
@@ -179,10 +268,57 @@ public class QueryBuilder
     }
 
     /**
+     * Sets the table you are updating.
+     * @param table The name of the table.
+     * @return QueryBuilder
+     */
+    public QueryBuilder updateTable(String table)
+    {
+        setFromTable(table);
+        setQueryType("update");
+        return this;
+    }
+
+    /**
+     * Sets the table you are inserting values into.
+     * @param table The name of the table
+     * @return QueryBuilder
+     */
+    public QueryBuilder insertTable(String table)
+    {
+        setQueryType("insert");
+        setFromTable(table);
+        return this;
+    }
+
+    /**
+     * This assigns a single column to a single value.
+     * @param key The name of the column.
+     * @param value The value to set.
+     * @return QueryBuilder
+     */
+    public QueryBuilder setColumn(String key, Object value)
+    {
+        this.updateFields.put(key, value);
+        return this;
+    }
+
+    /**
+     * This assigns multiple columns at one time.
+     * @param map Map in the form of key => value pairs.
+     * @return QueryBuilder
+     */
+    public QueryBuilder setColumn(HashMap<String, Object> map)
+    {
+        this.updateFields.putAll(map);
+        return this;
+    }
+
+    /**
      * Adds a String column to the WHERE clause.
      * @param column The name of the column.
      * @param value The value of the column.
-     * @param comparisonOperator The comparison to use. <,>,=,<=,>=</=,>
+     * @param comparisonOperator The comparison to use. <,>,=,<=,>=,>
      * @return QueryBuilder
      */
     public QueryBuilder where(String column, Object value, String comparisonOperator)
@@ -193,6 +329,54 @@ public class QueryBuilder
             setWhereClauseSet(true);
         }
         this.whereClause += " " + column + " " + comparisonOperator + " ? ";
+        this.whereColumnValues.add(value);
+        this.columnType.add("String");
+        return this;
+    }
+
+    /**
+     * Used for handling queries with multiple where conditions.
+     * @param column The name of the column to check.
+     * @param value The value to check.
+     * @param comparisonOperator The comparison operator to use.
+     * @return QueryBuilder
+     */
+    public QueryBuilder and(String column, Object value, String comparisonOperator)
+    {
+        if(!isWhereClauseSet())
+        {
+            // Fallback for people who call things out of sequence.
+            this.whereClause = " WHERE " + column + " " + comparisonOperator + " ? ";
+            this.whereColumnValues.add(value);
+            this.columnType.add("String");
+            setWhereClauseSet(true);
+            return this;
+        }
+        this.whereClause += " AND " + column + " " + comparisonOperator + " ? ";
+        this.whereColumnValues.add(value);
+        this.columnType.add("String");
+        return this;
+    }
+
+    /**
+     * Used for handling queries with multiple where conditions.
+     * @param column The name of the column to check.
+     * @param value The value to check.
+     * @param comparisonOperator The comparison operator to use.
+     * @return QueryBuilder
+     */
+    public QueryBuilder or(String column, Object value, String comparisonOperator)
+    {
+        if(!isWhereClauseSet())
+        {
+            // Fallback for people who call things out of sequence.
+            this.whereClause = " WHERE " + column + " " + comparisonOperator + " ? ";
+            this.whereColumnValues.add(value);
+            this.columnType.add("String");
+            setWhereClauseSet(true);
+            return this;
+        }
+        this.whereClause += " OR " + column + " " + comparisonOperator + " ? ";
         this.whereColumnValues.add(value);
         this.columnType.add("String");
         return this;
@@ -234,8 +418,84 @@ public class QueryBuilder
 
     public void viewSQL()
     {
-        sql = "SELECT " + this.columns + " FROM " + this.fromTable + this.whereClause + this.orderClause + this.limitClause;
+        switch (this.queryType)
+        {
+            case "select":
+                selectQuery();
+                break;
+            case "update":
+                updateQuery();
+                break;
+            case "delete":
+                deleteQuery();
+                break;
+            case "insert":
+                insertQuery();
+                break;
+            default:
+                break;
+        }
         System.out.println("SQL: " + sql);
+    }
+
+
+    /**
+     * This performs operations on a database that are not of the select variety.
+     * @return The number of rows affected. This will not return a row insert id.
+     * @throws Exception If the query type is select or not supported.
+     */
+    public int executeOperation() throws Exception
+    {
+        try
+        {
+            switch (queryType)
+            {
+                case "update":
+                    updateQuery();
+                    break;
+                case "insert":
+                    insertQuery();
+                    break;
+                case "delete":
+                    deleteQuery();
+                    break;
+                case "select":
+                    throw new Exception("Must call executeQuery for select statements.");
+                default:
+                    throw new Exception("Invalid Query Type: " + getQueryType());
+            }
+            PreparedStatement preparedStatement = this.connection.prepareStatement(sql);
+            int x = 1;
+            for(String key : updateFields.keySet())
+            {
+                try
+                {
+                    preparedStatement.setObject(x, this.updateFields.get(key));
+                }
+                catch (Exception e)
+                {
+                    // Currency fixing.
+                    String myNumberFormat = NumberFormat.getCurrencyInstance(Locale.US).format(this.updateFields.get(x));
+                    System.out.println(myNumberFormat);
+                    //Number newNumber = myNumberFormat.parse((String) this.updateFields.get(key));
+                    preparedStatement.setString(x, myNumberFormat);
+                }
+                ++x;
+            }
+            for (Object column : whereColumnValues)
+            {
+                preparedStatement.setObject(x, column);
+                x++;
+            }
+            int rowsAffected = preparedStatement.executeUpdate();
+            setResultSetMetaData(preparedStatement.getMetaData());
+            return rowsAffected;
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     /**
@@ -248,7 +508,7 @@ public class QueryBuilder
     public ArrayList<Object> executeQuery()
     {
         ArrayList<Object> objectArrayList = new ArrayList<>();
-        sql = "SELECT " + this.columns + " FROM " + this.fromTable + this.whereClause + this.orderClause + this.limitClause;
+        selectQuery();
 
         try
         {
@@ -273,7 +533,7 @@ public class QueryBuilder
                 Class<?> annotatedClass = Class.forName(this.className);
                 Field[] fields = annotatedClass.getDeclaredFields();
                 Object obj = null;
-                Constructor constructors[] = annotatedClass.getConstructors();
+                Constructor[] constructors = annotatedClass.getConstructors();
                 for(Constructor constructor : constructors)
                 {
                     if(constructor.getParameterCount() == 0)
@@ -319,6 +579,7 @@ public class QueryBuilder
                         }
                     }
                 }
+                assert obj != null;
                 System.out.println(obj.toString());
                 objectArrayList.add(obj);
             }
